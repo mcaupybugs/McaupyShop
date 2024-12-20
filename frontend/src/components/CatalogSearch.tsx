e02b8f9437d7827f8107d998af169f6cabf0d7af
Vishal Yadav <44060199+mcaupybugs@users.noreply.github.com>
2024-12-20 23:52:41 +0530

    Adding side catalog

--- frontend/src/components/CatalogSearch.tsx
+++ frontend/src/components/CatalogSearch.tsx
@@ -1,6 +1,6 @@
 const CatalogSearch = () => {
   return (
-    <div class="h-full w-full pt-12 border border-black rounded-sm flex-row font-Jost text-2xl font-light justify-end">
+    <div class="h-content w-full border border-black rounded-sm flex-row font-Jost text-2xl font-light justify-end border-t-0">
       <div class="flex flex-row justify-between p-2 border border-black border-l-0 border-r-0">
         <div class="flex">1-4 of 4 products</div>
         <div class="flex underline">Clear</div>
@@ -13,6 +13,30 @@ const CatalogSearch = () => {
             class="w-full rounded-md p-2 border border-black focus:outline-none"
           />
         </div>
+      </div>
+      {/* Create this dropdown using tailwind css */}
+      <div class="flex flex-col p-3 border border-black border-x-0 border-t-0">
+        <button
+          data-dropdown-toggle="dropdown"
+          class="flex justify-between"
+          type="button"
+        >
+          Sort By
+        </button>
+        <div id="dropdown" class="hidden">
+          <ul>
+            <li>Newest</li>
+            <li>Oldest</li>
+            <li>Price: Low to High</li>
+            <li>Price: High to Low</li>
+          </ul>
+        </div>
+      </div>
+      <div class="flex flex-col p-3 border border-black border-x-0 border-t-0">
+        Tags
+      </div>
+      <div class="flex flex-col p-3 border border-black border-x-0 border-t-0 border-b-0">
+        Price
       </div>
     </div>
   );
