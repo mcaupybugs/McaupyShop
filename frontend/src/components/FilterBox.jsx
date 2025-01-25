d7ac72fe1ccd2a544b9213fc12775e3c3f1426ab
Vishal Yadav <vishalydv.me@gmail.com>
2025-01-26 02:45:27 +0530

    Adding filterBox and project template

---
+++ frontend/src/components/FilterBox.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const FilterBox = () => {
+    return (
+        <div className='h-full w-full border'>
+            Filter box
+        </div>
+    )
+}
+
+export default FilterBox;
--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -1,5 +1,7 @@
 import React from 'react';
 import Navbar from '../components/Navbar';
+import FilterBox from '../components/FilterBox';
+import Project from '../components/Project';
 
 const HomePage = () => {
     return (
@@ -11,28 +13,28 @@ const HomePage = () => {
             <div className='h-full w-full flex flex-col'>
                 {/* horizontal diving the page into three sections to ensure margins on both ends */}
                 <div className='h-42 w-full flex flex-row border-b'>
-                    <div className='h-full w-full basis-2xl'>
+                    <div className='h-full w-full basis-3/14'>
 
                     </div>
-                    <div className='h-full w-full flex flex-col'>
+                    <div className='h-full w-full flex flex-col basis-9/14'>
                         <div className='h-full w-full text-2xl pt-4 font-light'>
                             I am Vishal, coding tag McaupyBugs, I love to develop stuff which I post here, available to be used. Basically it is a portal to my world, and my day to day coding shenanigans.
                         </div>
                     </div>
-                    <div className='h-full w-full basis-xl'>
+                    <div className='h-full w-full basis-2/14'>
 
                     </div>
                 </div>
                 <div className='h-full w-full flex flex-row'>
-                    <div className='h-full w-full basis-2xl'>
+                    <div className='h-full w-full basis-3/14'>
 
                     </div>
-                    <div className='h-full w-full flex flex-col'>
+                    <div className='h-full w-full flex flex-col basis-9/14'>
                         <div className='h-full w-full text-3xl pt-4 font-small'>
                             Catalog
                         </div>
                     </div>
-                    <div className='h-full w-full basis-xl'>
+                    <div className='h-full w-full basis-2/14'>
 
                     </div>
                 </div>
@@ -42,7 +44,8 @@ const HomePage = () => {
                     </div>
                     <div className='h-full w-full flex flex-col'>
                         <div className='h-full w-full text-3xl pt-4 font-small'>
-                            Bottom content
+                            <FilterBox></FilterBox>
+                            <Project></Project>
                         </div>
                     </div>
                     <div className='h-full w-full basis-xl'>
