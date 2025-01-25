8d491f9805dc9716d03a3e9208723e3cb19324b1
Vishal Yadav <vishalydv.me@gmail.com>
2025-01-26 02:37:23 +0530

    Adding the hero section

--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -3,8 +3,53 @@ import Navbar from '../components/Navbar';
 
 const HomePage = () => {
     return (
-        <div>
-            <Navbar></Navbar>
+        <div className='h-full w-full flex flex-col'>
+            <div>
+                <Navbar></Navbar>
+            </div>
+            {/* rest of the page, after the navbar vertically */}
+            <div className='h-full w-full flex flex-col'>
+                {/* horizontal diving the page into three sections to ensure margins on both ends */}
+                <div className='h-42 w-full flex flex-row border-b'>
+                    <div className='h-full w-full basis-2xl'>
+
+                    </div>
+                    <div className='h-full w-full flex flex-col'>
+                        <div className='h-full w-full text-2xl pt-4 font-light'>
+                            I am Vishal, coding tag McaupyBugs, I love to develop stuff which I post here, available to be used. Basically it is a portal to my world, and my day to day coding shenanigans.
+                        </div>
+                    </div>
+                    <div className='h-full w-full basis-xl'>
+
+                    </div>
+                </div>
+                <div className='h-full w-full flex flex-row'>
+                    <div className='h-full w-full basis-2xl'>
+
+                    </div>
+                    <div className='h-full w-full flex flex-col'>
+                        <div className='h-full w-full text-3xl pt-4 font-small'>
+                            Catalog
+                        </div>
+                    </div>
+                    <div className='h-full w-full basis-xl'>
+
+                    </div>
+                </div>
+                <div className='h-full w-full flex flex-row '>
+                    <div className='h-full w-full basis-2xl'>
+
+                    </div>
+                    <div className='h-full w-full flex flex-col'>
+                        <div className='h-full w-full text-3xl pt-4 font-small'>
+                            Bottom content
+                        </div>
+                    </div>
+                    <div className='h-full w-full basis-xl'>
+
+                    </div>
+                </div>
+            </div>
         </div>
     )
 }
